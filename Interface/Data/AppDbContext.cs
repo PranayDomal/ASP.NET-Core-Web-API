b59f877d910d3cf2cfadcf157d05d7a21273c856
Pranay Domal <domalpranay@gmail.com>
2026-09-11 06:12:08 +0530

    Add files via upload

---
+++ Interface/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using District_webapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace District_webapi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<District> District { get; set; }
+    }
+}
---
+++ Interface/Models/District.cs
@@ -0,0 +1,10 @@
+﻿namespace District_webapi.Models
+{
+    public class District
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountryId { get; set; }
+        public int StateId { get; set; }
+    }
+}
